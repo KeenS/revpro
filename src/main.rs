dc7083b24a72d020a61d1a56b85d1706a5765667
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-06-12 14:29:32 +0900

    use handler

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,29 @@ use hyper::server::Response;
 use hyper::net::Fresh;
 use std::io::Read;
 use hyper::Client;
-use hyper::header::Connection;
+//use hyper::header::Connection;
+use hyper::server::Handler;
+
+struct HTTPHandler;
 
 
+impl Handler for HTTPHandler {
+    fn handle(&self, _: Request, res: Response<Fresh>){
+        let mut res = res.start().unwrap();
 
-fn hello(_: Request, res: Response<Fresh>){
-    let mut res = res.start().unwrap();
-
-    let mut client = Client::new();
-    let mut content = client.get("http://localhost:8080")
-        .send().unwrap();;
-    let mut body = Vec::new();
-    content.read_to_end(&mut body).unwrap();
-    println!("{:?}", body);
-    res.write_all(&body[..]).unwrap();
-    res.end().unwrap();
+        let mut client = Client::new();
+        let mut content = client.get("http://localhost:8080")
+            .send().unwrap();;
+        let mut body = Vec::new();
+        content.read_to_end(&mut body).unwrap();
+        println!("{:?}", body);
+        res.write_all(&body[..]).unwrap();
+        res.end().unwrap();
+    }
 }
 
+
 fn main(){
-    Server::http(hello).listen("127.0.0.1:3000").unwrap();
+    Server::http(HTTPHandler).listen("127.0.0.1:3000").unwrap();
 
 }
